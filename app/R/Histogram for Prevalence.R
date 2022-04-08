a85a6a5f7ddb2f975178deb23ea79c7b441db1a2
i4h2b <i4h2b@ugrad.cs.ubc.a>
2022-04-07 20:01:20 -0700

    Add code for axis label sizes

--- app/R/Histogram for Prevalence.R
+++ app/R/Histogram for Prevalence.R
@@ -8,4 +8,6 @@ ggplot(cwd,aes(x=Year,y=Prevalence,fill=Year))+
   ylab("CWD Prevalence (%)")+
   theme_classic()+
   scale_x_continuous(breaks=c(2006:2019))+
-  theme(legend.position="none")
+  theme(legend.position="none")+
+  theme(axis.text=element_text(size=14),
+        axis.title=element_text(size=16,face="bold"))
